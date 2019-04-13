1c6b239337c45ae58198fea785b9d0fe4be8bfd1
Fatih Canbekli <fatih.canbekli@outlook.com>
2019-04-13 08:22:27 +0300

    Logo and UI regulation added

--- Instagram Clone/Instagram Clone/Controller/LoginViewController.swift
+++ Instagram Clone/Instagram Clone/Controller/LoginViewController.swift
@@ -12,6 +12,19 @@ class LoginViewController: UIViewController {
 
     // MARK - Properities
     
+    let logoContainerView: UIView = {
+        let view = UIView()
+        let logoImageView = UIImageView(image: #imageLiteral(resourceName: "Instagram_logo_white"))
+        logoImageView.contentMode = .scaleAspectFill
+        view.addSubview(logoImageView)
+        logoImageView.anchor(top: nil, left: nil, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 200, height: 50)
+        logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        view.backgroundColor = UIColor(red: 0/255, green: 120/255, blue: 175/255, alpha: 1)
+        return view
+    }()
+    
+    
     let emailTextField: UITextField = {
         let tf = UITextField()
         tf.placeholder = "Email"
@@ -35,10 +48,9 @@ class LoginViewController: UIViewController {
         button.setTitle("Login", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
-        button.layer.cornerRadius = 5 // Control Button Shape!!
+        button.isEnabled = false
+        button.layer.cornerRadius = 5
         return button
-        
-        
     }()
     
     // MARK - Init
@@ -47,6 +59,10 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        view.addSubview(logoContainerView)
+        logoContainerView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 150)
+        
         configureViewComponents()
 
         // Do any additional setup after loading the view.
@@ -54,7 +70,7 @@ class LoginViewController: UIViewController {
     
     // MARK - Handler
     
-    func configureViewComponents(){
+    func configureViewComponents() {
         
         let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton])
         
@@ -63,7 +79,6 @@ class LoginViewController: UIViewController {
         stackView.distribution = .fillEqually
         
         view.addSubview(stackView)
-        stackView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 40, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 140)
+        stackView.anchor(top: logoContainerView.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 40, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 140)
     }
-    
 }
--- Instagram Clone/Instagram Clone/Utils/Extensions.swift
+++ Instagram Clone/Instagram Clone/Utils/Extensions.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 extension UIView {
-    func anchor(top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?, paddingTop: CGFloat ,paddingLeft: CGFloat,paddingBottom: CGFloat,paddingRight: CGFloat, width: CGFloat, height: CGFloat ) {
+    
+    func anchor(top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat) {
+        
         translatesAutoresizingMaskIntoConstraints = false
         
         if let top = top {
@@ -19,21 +21,21 @@ extension UIView {
         if let left = left {
             self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
         }
-
+        
         if let bottom = bottom {
-            self.bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
+            self.bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
         }
-
+        
         if let right = right {
-            self.rightAnchor.constraint(equalTo: right, constant: paddingRight).isActive = true
+            self.rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
         }
-
+        
         if width != 0 {
             widthAnchor.constraint(equalToConstant: width).isActive = true
         }
         
         if height != 0 {
-            widthAnchor.constraint(equalToConstant: height).isActive = true
+            heightAnchor.constraint(equalToConstant: height).isActive = true
         }
     }
 }
